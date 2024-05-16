4263580794499f569ad84349112693f03253aba7
Victoria Pelaez <victoria.pelaez@devsense.io>
2024-05-16 17:50:28 +0200

    change all clases, better names

--- script.js
+++ script.js
@@ -1,33 +1,33 @@
 document.addEventListener("DOMContentLoaded", function () {
   // Hacer que el modal 1 aparezca después de 1 segundo
   setTimeout(function () {
-    var modal1 = document.querySelector(".modal-1");
+    var modal1 = document.querySelector(".banner");
     modal1.style.visibility = "visible";
     modal1.style.opacity = "1";
   }, 1000);
 
-  document.querySelector(".accept").addEventListener("click", function (event) {
+  document.querySelector(".button-accept").addEventListener("click", function (event) {
     event.preventDefault();
-    document.querySelector(".modal").style.display = "none";
+    document.querySelector(".banner").style.display = "none";
   });
 
-  document.querySelector(".preferences").addEventListener("click", function (event) {
-    var modal2 = document.querySelector(".modal-2");
+  document.querySelector(".button-preferences").addEventListener("click", function (event) {
+    var modal2 = document.querySelector(".purposes-banner");
     event.preventDefault();
-    document.querySelector(".modal-1").style.display = "none";
-    document.querySelector(".modal-2").classList.remove("hidden");
+    document.querySelector(".banner").style.display = "none";
+    document.querySelector(".purposes-banner").classList.remove("hidden");
     modal2.style.visibility = "visible";
     modal2.style.opacity = "1";
   });
 
-  document.querySelector(".save").addEventListener("click", function (event) {
+  document.querySelector(".button-save").addEventListener("click", function (event) {
     event.preventDefault();
-    document.querySelector(".modal-2").style.display = "none";
+    document.querySelector(".purposes-banner").style.display = "none";
   });
 
-  var closeButton = document.querySelector(".close-btn");
+  var closeButton = document.querySelector(".button-x-close");
   closeButton.addEventListener("click", function () {
     event.preventDefault();
-    document.querySelector(".modal-2").style.display = "none";
+    document.querySelector(".purposes-banner").style.display = "none";
   });
 });

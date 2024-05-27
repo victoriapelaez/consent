783c379a3134df9c46275afadef4a153fce51175
Victoria Pelaez <victoria.pelaez@devsense.io>
2024-05-27 10:33:05 +0200

    fix js

--- script.js
+++ script.js
@@ -11,30 +11,39 @@ document.addEventListener("DOMContentLoaded", function () {
     document.querySelector(selector).style.display = "none";
   }
 
-
   function handleHideButtonClick(buttonSelectors, elementToHide) {
     buttonSelectors.forEach(function (buttonSelector) {
-      document.querySelector(buttonSelector).addEventListener("click", function (event) {
-        event.preventDefault();
-        hideElement(elementToHide);
-      });
+      var e = document.querySelector(buttonSelector);
+      if (e) {
+        e.addEventListener("click", function (event) {
+          event.preventDefault();
+          hideElement(elementToHide);
+        });
+      }
     });
   }
 
   showElementAfterDelay(".banner", 1000);
 
-  document.querySelector(".button-preferences").addEventListener("click", function (event) {
-    event.preventDefault();
-    hideElement(".banner");
-    var modal2 = document.querySelector(".purposes-banner");
-    modal2.classList.remove("hidden");
-    modal2.style.visibility = "visible";
-    modal2.style.opacity = "1";
-  });
+  document
+    .querySelector(".button-preferences")
+    .addEventListener("click", function (event) {
+      event.preventDefault();
+      hideElement(".banner");
+      var modal2 = document.querySelector(".purposes-banner");
+      modal2.classList.remove("hidden");
+      modal2.style.visibility = "visible";
+      modal2.style.opacity = "1";
+    });
 
-  handleHideButtonClick([".button-accept", ".button-decline", ".button-decline-all", ".button-accept-all"], ".banner");
-
-  handleHideButtonClick([".button-x-close", ".button-save"], ".purposes-banner");
-
+  handleHideButtonClick([".button-accept", ".button-decline"], ".banner");
+  handleHideButtonClick(
+    [
+      ".button-x-close",
+      ".button-save",
+      ".button-decline-all",
+      ".button-accept-all",
+    ],
+    ".purposes-banner"
+  );
 });
-
